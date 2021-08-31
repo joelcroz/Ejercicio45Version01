29fb0fee2c3929b80b191a5045bc0a047eafd164
Joelcrosz <joelcisneros528@gmail.com>
2021-08-30 22:38:57 -0300

    Ejercicio Completo

--- Ejercicio45Version01/Program.cs
+++ Ejercicio45Version01/Program.cs
@@ -10,6 +10,18 @@ namespace Ejercicio45Version01
     {
         static void Main(string[] args)
         {
-        }
+
+            var suma = 0;
+			suma = 0;
+            for (int contador = 1; contador <= 5; contador++)
+            {
+				Console.Write($"Ingrese el {contador}º nùmero:");
+                int numeroIngresado = Convert.ToInt32(Console.ReadLine());
+                suma += numeroIngresado;
+            }
+
+            Console.WriteLine($"El resultado de la suma es {suma}");
+            Console.ReadLine();
+		}
     }
 }
